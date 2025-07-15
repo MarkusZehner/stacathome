fd34a6621ddfd8516d470528658b9acb489229ed
Sebastian Hoffmann <shoffmann.git@gmail.com>
2025-07-15 11:27:40 +0200

    feat: refactored geo.py to use geom.Geometry

--- stacathome/geo.py
+++ stacathome/geo.py
@@ -1,63 +1,78 @@
-import numpy as np
 from functools import partial
+
+import numpy as np
+import odc.geo.geom as geom
 import pyproj
 import pystac
 import shapely
-import odc.geo.geom
 
 _WGS84_GEOD = pyproj.Geod(ellps='WGS84')
-_PROJ_WGS84 = pyproj.Proj('EPSG:4326')
-_PROJ_LAMBERT = pyproj.Proj('EPSG:9835')  # Lambert Cylindrical Equal Area
-_EQUAL_AREA_TRANSFORMER = pyproj.Transformer.from_proj(_PROJ_WGS84, _PROJ_LAMBERT, always_xy=True)
 
 
-def wgs84_to_equal_area(geometry: shapely.Geometry) -> shapely.Geometry:
+def to_equal_area(geometry: geom.Geometry) -> geom.Geometry:
     """
-    Transform a geometry from WGS 84 (EPSG:4326) to Lambert Cylindrical Equal Area (EPSG:9835).
+    Transform a geometry to Lambert Cylindrical Equal Area (EPSG:9835).
     This is useful for area calculations in a projected coordinate system.
 
     Args:
-        geometry (shapely.Geometry): The geometry to transform.
+        geometry (Geometry): The geometry to transform.
     Returns:
-        shapely.Geometry: The transformed geometry in Lambert Cylindrical Equal Area (EPSG:9835).
+        Geometry: The transformed geometry in Lambert Cylindrical Equal Area (EPSG:9835).
     """
-    return shapely.transform(geometry, _EQUAL_AREA_TRANSFORMER.transform, interleaved=False)
+    return geometry.to_crs('EPSG:9835')
 
 
-def wgs84_centroid(geometry: odc.geo.geom.Geometry) -> shapely.Point:
+def to_wgs84(geometry: geom.Geometry) -> geom.Geometry:
+    """
+    Transform a geometry to WGS 84 (EPSG:4326).
+
+    Args:
+        geometry (Geometry): The geometry to transform.
+    Returns:
+        Geometry: The transformed geometry in WGS 84 (EPSG:4326).
+    """
+    return geometry.to_crs('EPSG:4326')
+
+
+def wgs84_centroid(geometry: geom.Geometry) -> geom.Geometry:
     """
     Calculate the centroid of a geometry in WGS 84 (EPSG:4326) coordinate reference system.
     This function takes into account the geometry of the spheroid and returns the centroid in latitude and longitude.
 
     Args:
-        geometry (shapely.Geometry): The geometry for which to calculate the centroid.
+        geometry (Geometry): The geometry for which to calculate the centroid.
     Returns:
         shapely.Point: The centroid of the geometry in WGS 84 (EPSG:4326) coordinate reference system.
     """
     # TODO: Implement "A New Method for Finding Geographic Centers, with Application to U.S. States", Peter A. Rogerson
-    return shapely.Point(*geometry.centroid.points)
+    return geometry.centroid.to_crs('EPSG:4326')
 
 
-def wgs84_geodesic_distance(point1: shapely.Point, point2: shapely.Point) -> float:
+def wgs84_geodesic_distance(point1: geom.Geometry, point2: geom.Geometry) -> float:
     """
     Calculate the distance between two points in WGS 84 (EPSG:4326) coordinate reference system.
-    The points are assumed to be in latitude and longitude.
 
     Args:
-        point1 (shapely.Point): The first point.
-        point2 (shapely.Point): The second point.
+        point1 (Geometry): The first point.
+        point2 (Geometry): The second point.
     Returns:
         float: The distance between the two points in meters.
     """
-    return _WGS84_GEOD.line_length([point1.x, point2.x], [point1.y, point2.y])
+    if point1.geom_type != 'Point' or point2.geom_type != 'Point':
+        raise ValueError(f"Only point geometries are accepted. Got: '{point1.geom_typ}' and '{point2.geom_typ}'")
+    point1 = to_wgs84(point1)
+    point2 = to_wgs84(point2)
+    xx = (shapely.get_x(point1.geom), shapely.get_x(point2.geom))
+    yy = (shapely.get_y(point1.geom), shapely.get_y(point2.geom))
+    return _WGS84_GEOD.line_length(xx, yy)
 
 
-def centroid_distance(shape1: odc.geo.geom.Geometry, shape2: odc.geo.geom.Geometry) -> float:
+def centroid_distance(shape1: geom.Geometry, shape2: geom.Geometry) -> float:
     """
-    Calculate the distance between the centroids of two shapes. The shapes are assumed to be in WGS 84 (EPSG:4326) coordinate reference system.
+    Calculate the geodesic distance between the centroids of two shapes with regard to the WGS 84 reference ellipsoid.
     Args:
-        shape1 (shapely.Geometry): The first shape.
-        shape2 (shapely.Geometry): The second shape.
+        shape1 (Geometry): The first shape.
+        shape2 (Geometry): The second shape.
     Returns:
         float: The distance between the centroids of the two shapes in meters.
     """
@@ -66,36 +81,36 @@ def centroid_distance(shape1: odc.geo.geom.Geometry, shape2: odc.geo.geom.Geomet
     return wgs84_geodesic_distance(centroid1, centroid2)
 
 
-def wgs84_IoU(shape1: shapely.Geometry, shape2: shapely.Geometry) -> float:
+def wgs84_IoU(shape1: geom.Geometry, shape2: geom.Geometry) -> float:
     """
     Calculate the Intersection over Union (IoU) percentage between two shapes in WGS 84 (EPSG:4326) coordinate reference system.
     Args:
-        shape1 (shapely.Geometry): The first shape.
-        shape2 (shapely.Geometry): The second shape.
+        shape1 (Geometry): The first shape.
+        shape2 (Geometry): The second shape.
     Returns:
         float: The IoU percentage between the two shapes. Returns 0.0 if there is no intersection.
     """
-    shape1 = wgs84_to_equal_area(shape1)
-    shape2 = wgs84_to_equal_area(shape2)
+    shape1 = to_equal_area(shape1)
+    shape2 = to_equal_area(shape2)
     union = shape1.union(shape2)
     intersection = shape1.intersection(shape2)
     return intersection.area / union.area if intersection.area > 0 else 0.0
 
 
-def wgs84_overlap_percentage(shape1: shapely.Geometry, shape2: shapely.Geometry) -> float:
+def overlap_percentage(shape1: geom.Geometry, shape2: geom.Geometry) -> float:
     """
     Calculate the overlap percentage (measured from the smaller shape) between two shapes in WGS 84 (EPSG:4326) coordinate reference system.
 
     Args:
-        shape1 (shapely.Geometry): The first shape.
-        shape2 (shapely.Geometry): The second shape.
+        shape1 (Geometry): The first shape.
+        shape2 (Geometry): The second shape.
 
     Returns:
         float: The overlap percentage between the two shapes. 0.0 if there is no intersection, 1.0 if one shape is fully contained in the other.
     """
 
-    shape1 = wgs84_to_equal_area(shape1)
-    shape2 = wgs84_to_equal_area(shape2)
+    shape1 = to_equal_area(shape1)
+    shape2 = to_equal_area(shape2)
     intersection = shape1.intersection(shape2)
     if intersection.is_empty:
         return 0.0
@@ -103,13 +118,13 @@ def wgs84_overlap_percentage(shape1: shapely.Geometry, shape2: shapely.Geometry)
     return intersection.area / area if area > 0 else 0.0
 
 
-def wgs84_contains(shape1: odc.geo.geom.Geometry, shape2: odc.geo.geom.Geometry, local_proj_code: str) -> bool:
+def wgs84_contains(shape1: geom.Geometry, shape2: geom.Geometry, local_proj_code: str) -> bool:
     """
     Check if one shape fuly contains another shape in WGS 84 (EPSG:4326) coordinate reference system if projected to a local coordinate system.
 
     Args:
-        shape1 (shapely.Geometry): The first shape.
-        shape2 (shapely.Geometry): The second shape.
+        shape1 (Geometry): The first shape.
+        shape2 (Geometry): The second shape.
         local_proj_code (str): The local projection code to transform the shapes before checking containment.
 
     Returns:
--- test/test_geo.py
+++ test/test_geo.py
@@ -1,4 +1,6 @@
 import shapely
+
+import odc.geo.geom as geom
 from stacathome.geo import wgs84_centroid, wgs84_geodesic_distance
 
 
@@ -8,9 +10,9 @@ class TestGeo:
         """
         Test the wgs84_geodesic_distance function.
         """
-        point1 = shapely.Point(0, 0)
-        point2 = shapely.Point(1.0, 0)
-        point3 = shapely.Point(0, 1.0)
+        point1 = geom.point(0, 0, crs='EPSG:4326')
+        point2 = geom.point(1.0, 0, crs='EPSG:4326')
+        point3 = geom.point(0, 1.0, crs='EPSG:4326')
 
         # same point
         assert wgs84_geodesic_distance(point1, point1) < 1e-9
