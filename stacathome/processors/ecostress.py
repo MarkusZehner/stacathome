d11fa7186fac051e486dceed022cb37e51f672d4
markuszehner <mzehner.mz@gmail.com>
2025-07-28 15:40:51 +0200

    feat: load xr from earthaccess, raster ext optional in update items

--- stacathome/processors/ecostress.py
+++ stacathome/processors/ecostress.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from pathlib import Path
 
+import xarray as xr
 import rasterio
 from rasterio.env import Env
 import pystac
@@ -11,6 +12,7 @@ from rio_stac.stac import (
     get_raster_info,
 )
 from odc.geo import geom
+from odc.geo.geobox import GeoBox
 
 from ..providers.common import BaseProvider
 from ..stac import update_asset_exts
@@ -30,7 +32,7 @@ class ECO_L2T_LSTEProcessor(BaseProcessor):
     
     
     
-    '''
+    ''' 
     def filter_items(
         self, provider: BaseProvider, roi: geom.Geometry, items: pystac.ItemCollection
     ) -> pystac.ItemCollection:
@@ -52,6 +54,53 @@ class ECO_L2T_LSTEProcessor(BaseProcessor):
             clone_items=False,
             extra_fields=extra_fields,
         )
+        
+    def load_items_geoboxed(
+        self,
+        provider: BaseProvider,
+        geobox: GeoBox,
+        items: pystac.ItemCollection,
+        variables: list[str] | None = None,
+        resampling: dict[str, str] | None = None,
+        dtype: dict[str, float] | None = None,
+    ) -> xr.Dataset:
+        """
+        Download items in the collection.
+        :param provider: The provider to use for downloading.
+        :param geobox: The geobox defining the spatial extent and CRS of the output.
+        :param items: The item collection to download.
+        :return: Item collection with downloaded items.
+        """
+        if not items:
+            raise ValueError('No items provided')
+        variables = set(variables) if variables else None
+        
+        with Env(
+            **handle_rasterio_env()
+        ):
+            return provider.load_items(
+                items,
+                geobox=geobox,
+                variables=variables,
+                resampling=resampling,
+                dtype=dtype,
+                )
+
+
+def handle_rasterio_env() -> dict:
+    cookie_file_urs = Path.home() / '.urs_cookies'
+    if not Path.is_file(cookie_file_urs):
+        raise FileNotFoundError('no cookie file for earthaccess found.')
+    return {
+        'GDAL_HTTP_COOKIEFILE' : cookie_file_urs,
+        'GDAL_DISABLE_READDIR_ON_OPEN' : 'YES',
+        'GDAL_HTTP_TCP_KEEPALIVE' : 'YES',
+        'GDAL_PAM_ENABLED' :'NO',
+        'GDAL_MAX_DATASET_POOL_SIZE' :'1',
+        'CPL_VSIL_CURL_USE_HEAD' :'NO',
+        'VSI_CACHE' :'YES',
+        'VSI_CACHE_SIZE' :100 * 1024 * 1024,
+        }
 
 def ecostress_pc_filter_newest_processing_iteration(items: list[pystac.Item]) -> list[pystac.Item]:
     """
@@ -67,11 +116,13 @@ def ecostress_pc_filter_newest_processing_iteration(items: list[pystac.Item]) ->
             filtered[base_name] = (product_iteration, item)
     return [v[1] for v in filtered.values()]
 
-def ecostress_update_from_cloud(items: list[pystac.Item]) -> list[pystac.Item]:
+def ecostress_update_from_cloud(items: list[pystac.Item], proj=True, raster = False) -> list[pystac.Item]:
     """
     Gathers additional infromation by reading the metadata from the provider.
     To limit this, items are grouped by their UTM zone and all items of one zone are assumed to have the same proj and raster info.
     The ID is expected to be in the format 'ECOv002_L2T_LSTE_28425_001_32MQE_20230711T175148_0710_01'.
+    
+    using both proj and raster takes very long
     """
     
     utm_dict = defaultdict(list)
@@ -80,55 +131,52 @@ def ecostress_update_from_cloud(items: list[pystac.Item]) -> list[pystac.Item]:
         utm_dict[utm_zone].append(item)
 
     info_dict = {}
-    cookie_file_urs = Path.home() / '.urs_cookies'
-    if Path.is_file(cookie_file_urs):
-        with Env(
-            GDAL_HTTP_COOKIEFILE=cookie_file_urs,
-            GDAL_DISABLE_READDIR_ON_OPEN='YES',
-            GDAL_HTTP_TCP_KEEPALIVE = 'YES',
-            GDAL_PAM_ENABLED='NO',          
-            GDAL_MAX_DATASET_POOL_SIZE='1', 
-            CPL_VSIL_CURL_USE_HEAD='NO',
-            # CPL_CURL_VERBOSE='YES',
-            VSI_CACHE='YES',
-            VSI_CACHE_SIZE=100 * 1024 * 1024,
-        ):
-            for utm_zone, item_list in utm_dict.items():
-                info_dict[utm_zone] = {}
-                proj_info = {}
-                for name, asset in item_list[0].get_assets().items():
-                    info_dict[utm_zone][name] = {}
-                    if proj_info == {}:
-                        with rasterio.open(asset.href) as src_dst:
+    with Env(
+        **handle_rasterio_env()
+    ):
+        raster_info = {}
+        for utm_zone, item_list in utm_dict.items():
+            info_dict[utm_zone] = {}
+            proj_info = {}
+            
+            for name, asset in item_list[0].get_assets().items():
+                info_dict[utm_zone][name] = {}
+                print(not raster_info.get(name), raster, raster and not raster_info.get(name))
+                if not proj_info or (raster and not raster_info.get(name)):
+                    with rasterio.open(asset.href) as src_dst:
+                        if not proj_info:
+                            print(f'get {utm_zone} proj info')
                             proj_info_set = get_projection_info(src_dst).items()
                             proj_info = {
-                                f"proj:{name}": value
-                                for name, value in proj_info_set if name in 
+                                f"proj:{pname}": value
+                                for pname, value in proj_info_set if pname in
                                 ['epsg', 'code', 'bbox', 'shape', 'transform']
                             }
                             if 'proj:epsg' in proj_info and not 'proj:code' in proj_info:
                                 proj_info['proj:code'] = proj_info['proj:epsg']
                                 del proj_info['proj:epsg']
-                        # raster_info = {
-                        #     "raster:bands": get_raster_info(src_dst, max_size=1024)
-                        # }
-                        # info_dict[utm_zone][name] = raster_info
-                info_dict[utm_zone] = info_dict[utm_zone] | proj_info
-                    
+                        if raster and not raster_info.get(name):
+                            print(f'get {utm_zone} {name} raster info')
+                            raster_info[name] = {
+                                "raster:bands": get_raster_info(src_dst, max_size=1024)
+                            }
+            info_dict[utm_zone] = info_dict[utm_zone] | proj_info
+        info_dict = info_dict | raster_info
+
 
     items_with_exts = []                            
     for utm_zone, item_list in utm_dict.items():
         for item in item_list:
             ProjectionExtension.add_to(item)
-            #RasterExtension.add_to(item)
+            RasterExtension.add_to(item)
             proj_ext = ProjectionExtension.ext(item)
             proj_ext.epsg = info_dict[utm_zone]['proj:code']
             proj_ext.bbox = info_dict[utm_zone]['proj:bbox']
             proj_ext.shape = info_dict[utm_zone]['proj:shape']
             proj_ext.transform = info_dict[utm_zone]['proj:transform']
-            # for name, asset in item.get_assets().items():
-            #     item.asset[name].extra_fields['raster:bands'] = info_dict[utm_zone][name]['raster:bands']
+            if raster:
+                for name, asset in item.get_assets().items():
+                    item.assets[name].extra_fields['raster:bands'] = info_dict[name]['raster:bands']
             item.properties['grid:code'] = utm_zone
             items_with_exts.append(item)
-            
     return items_with_exts
